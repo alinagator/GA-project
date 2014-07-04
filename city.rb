74515d3d8e056184c7a7774fe760dab805eaa935
Alina Solovjova <alina@gocardless.com>
2014-07-04 23:14:19 +0100

    Midterm v1

--- city.rb
+++ city.rb
@@ -45,3 +45,4 @@ class City
 
 end
 
+#changes
--- main.rb
+++ main.rb
@@ -5,3 +5,5 @@ puts "It's hot out there. Tell me where you are, and I'll tell you how hot it is
 city = City.new
 city.get_wu_data
 city.print_temp
+
+#changes
