989634c9356722412f183e4829971e484c270230
Alina Solovjova <alina@gocardless.com>
2014-07-05 01:26:26 +0100

    v2

--- main.rb
+++ main.rb
@@ -1,9 +1,28 @@
 require_relative 'city.rb'
 
-puts "It's hot out there. Tell me where you are, and I'll tell you how hot it is."
 
-city = City.new
-city.get_wu_data
-city.print_temp
+def welcome
+	puts "It's hot out there. Tell me where you are, and I'll tell you how hot it is."
+	create_city
+end
 
-#changes
+def create_city
+	city = City.new
+	city.get_wu_data
+	city.print_temp
+
+	puts "Want to know the temperature somewhere else?"
+	more_weather = gets.chomp.upcase
+
+	until (more_weather == "Y" or more_weather == "N")
+      puts "Please enter 'Y' or 'N'"
+      more_weather = gets.chomp.upcase
+    end
+
+    if more_weather =+ "Y"
+    	puts "Enter a new city."
+    	create_city
+    end
+end
+
+welcome
--- proposal.md
+++
@@ -1,7 +0,0 @@
-# My Proposal
-
-App that will list the books read in our book club + their reviews etc. 
-
-## Subheading
-
-Books books books.
